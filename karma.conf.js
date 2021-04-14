8e620e2194daa0776019c41cd6e37dcfa01c002b
Marcos Quintanilha <marcosoquintanilha@gmail.com>
2021-04-14 18:16:09 -0300

    Estrutura da esteira do teste de mutação.

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'cadastro/*.js',
+    //  'src/**/*.js',
+    //  'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'reports/coverage/',
+      reporters: [
+        { type: 'html', subdir: '.' }
+      ]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity
+  });
+}
